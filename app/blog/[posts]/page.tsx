b7bc77782595db5e92328725cfbeaf2a9638fbb0
Subham Choudhury <128825937+subhamc88@users.noreply.github.com>
2024-07-04 18:42:46 +0000

    page.tsx for posts added in the blog (#4)

---
+++ app/blog/[posts]/page.tsx
@@ -0,0 +1,26 @@
+import path from 'path'
+import { marked } from 'marked'
+import matter from 'gray-matter'
+import getPostMetadata from '../../../utils/getPostMetadata'
+import getFilePath from '../../../utils/getFilePath'
+
+const postDir = path.join(process.cwd(), '/posts');
+const filePath = getFilePath(postDir);
+
+export async function generateStaticParams() {
+	const metadata = getPostMetadata(filePath);
+
+	const postsLinks = metadata.map((posts) => (posts.title))
+	return postsLinks
+}
+
+export default function Posts(props: any) {
+	const title = decodeURI(props.params.posts) + '.md'
+	const filePath = path.join(postDir, title)
+	const { content } = matter.read(filePath)
+	const contentHtml = marked(content)
+
+	return (
+		<div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+	)
+}
