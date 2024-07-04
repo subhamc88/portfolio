275918e1c03b08f56afe413502bd29fc7fdb9c8a
subhamc88 <subham020304@gmail.com>
2024-07-04 17:12:50 +0000

    posts now rendered directly from markdown file

--- app/blog/[posts]/page.tsx
+++ app/blog/[posts]/page.tsx
@@ -1,7 +1,15 @@
-export default function Posts() {
+import path from 'path'
+import { marked } from 'marked'
+import matter from 'gray-matter'
+
+export default function Posts(props: any) {
+	const title = decodeURI(props.params.posts) + '.md'
+	const postDir = path.join(process.cwd(), 'posts/')
+	const filePath = path.join(postDir, title)
+	const { content } = matter.read(filePath)
+	const contentHtml = marked(content)
+
 	return (
-		<>
-			This is a post.
-		</>
+		<div dangerouslySetInnerHTML={{ __html: contentHtml }} />
 	)
 }
