72733daca260be2dacaecdccff27696bc99089d4
subhamc88 <subham020304@gmail.com>
2024-08-10 11:42:08 +0530

    animationSwipeRight added

---
+++ utils/animationSwipeRight.ts
@@ -0,0 +1,19 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+
+export default function animationSwipeRight() {
+	useGSAP(() => {
+		const timeline = gsap.timeline()
+
+		timeline.from(
+			'.page', { opacity: 0, duration: 0 })
+			.to('.overlay', {
+				x: '100%',
+				duration: 1.6
+			})
+			.to('.page', {
+				opacity: 1,
+				ease: 'power2.out'
+			}, "-=1.2")
+	}, [])
+}
