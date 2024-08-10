0489afebc7dca6bfd81b752b6f65091e6de6b748
subhamc88 <subham020304@gmail.com>
2024-08-10 23:51:51 +0530

    main page changed ( I don't know what to do with it)

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
 'use client'
-import animationSwipeRight from "../utils/animationSwipeRight";
-import animationSwipeLeft from "../utils/animationSwipeLeft";
 export default function Home() {
-	animationSwipeLeft()
 	return (
-
-		<main className="page bg-red-500 h-screen">
-			<div className="overlay bg-blue-400 w-screen h-5/6 absolute z-10"> Overlay </div>
-			Hello World
+		<main className="w-screen h-screen">
+			<h1>Don't know what to do here.</h1>
 		</main>
-	);
+	)
 }
