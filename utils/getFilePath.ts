d3699fa1b497d68a9dda3db8d763122148ebf2bd
subhamc88 <subham020304@gmail.com>
2024-08-11 00:46:47 +0530

    getFilePath improved with subdirectory searching

--- utils/getFilePath.ts
+++ utils/getFilePath.ts
@@ -1,19 +1,28 @@
 import fs from 'fs';
 import path from 'path';
 
-export default function getFilePath(dir: string) {
-	const filePath = []
-	const files = fs.readdirSync(dir);
+export default function getFilePath(postsDir: string) {
+	const dirPath: string[] = [];
+	const filePath: string[] = [];
 
-	// Search files inside the Directories.
-	for (let file of files) {
-		// File paths inside the Directories.
-		let bufferPath = path.join(dir, file);
-		// File existence Validation.
-		if (fs.existsSync(bufferPath)) {
-			filePath.push(bufferPath)
+	function searchFiles(dir: string) {
+		const entries = fs.readdirSync(dir);
+
+		for (let entry of entries) {
+			const fullPath = path.join(dir, entry);
+			const stat = fs.statSync(fullPath);
+			if (stat.isDirectory()) {
+				// If it's a directory, recursively search within it
+				searchFiles(fullPath);
+			} else if (stat.isFile() && path.extname(entry) === '.md') {
+				// If it's a .md file, add it to the filePath array
+				filePath.push(fullPath);
+			}
 		}
 	}
-	// Returns file path in array.
-	return filePath
+
+	// Start searching from the postsDir
+	searchFiles(postsDir);
+
+	return filePath;
 }
