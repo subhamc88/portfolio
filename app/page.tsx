fb8161bc9af1a88c38f38f78b575450b0a4543c1
subhamc88 <subham020304@gmail.com>
2024-08-10 11:43:29 +0530

    animations and stuff for testing added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+'use client'
+import animationSwipeRight from "../utils/animationSwipeRight";
+import animationSwipeLeft from "../utils/animationSwipeLeft";
 export default function Home() {
+	animationSwipeLeft()
 	return (
-		<main>
+
+		<main className="page bg-red-500 h-screen">
+			<div className="overlay bg-blue-400 w-screen h-5/6 absolute z-10"> Overlay </div>
+			Hello World
 		</main>
 	);
 }
