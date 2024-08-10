67949d7a90b961128e119ad9679776767428383c
subhamc88 <subham020304@gmail.com>
2024-08-10 23:48:24 +0530

    footer removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 import "./globals.css";
 
 const poppins = Poppins({
@@ -26,7 +25,6 @@ export default function RootLayout({
 			<body className={poppins.className}>
 				<Navbar />
 				{children}
-				<Footer />
 			</body>
 		</html >
 	);
--- components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Footer() {
-	return (
-		<footer className="text-left m-2 mt-10 text-gray-500">
-			<p>© 2024 subhamc88</p>
-		</footer>
-	)
-}
