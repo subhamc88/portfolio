8926c5a32d13db1e18bc3849a778389a582fe344
subhamc88 <subham020304@gmail.com>
2024-08-21 10:29:46 +0530

    some progress in blog and changing of colour scheme

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,31 +1,49 @@
-import path from "path"
-import Link from "next/link"
-import Image from "next/image"
-import getFilePath from "../../utils/getFilePath"
-import getPostMetadata from "../../utils/getPostMetadata"
+import path from "path";
+import Link from "next/link";
+import Image from "next/image";
+import getFilePath from "../../utils/getFilePath";
+import getPostMetadata from "../../utils/getPostMetadata";
 
 export default function Blog() {
-	const postDir = path.join(process.cwd(), '/posts')
+	const postDir = path.join(process.cwd(), '/posts');
 	const filePath = getFilePath(postDir);
 	const metadata = getPostMetadata(filePath);
 
 	return (
-		<ul className="overflow-y-scroll container flex-col justify-center items-center w-screen h-screen bg-blue-500">
-			<h1>This is the blog page</h1>
-			{metadata.map((post) => (
-				<li key={post.title}>
-					<Link href={`/blog/${post.title}`}>
-						<div className="flex-col bg-red-500 justify-around items-center m-10 rounded-3xl">
-							<Image
-								src={`/${post.image}`}
-								alt={post.title}
-								width={270}
-								height={100}
-							/>
-						</div>
-					</Link>
-				</li>
-			))}
-		</ul>
-	)
+		<div className="text-redCandy-black container mx-auto p-4 flex flex-wrap justify-center items-center min-h-screen">
+			<h1 className="text-3xl font-bold text-redCandy-black mb-8 w-full text-center">
+				Welcome to My Space
+			</h1>
+			<ul className="w-full flex flex-wrap justify-center">
+				{metadata.map((post) => (
+					<li
+						key={post.title}
+						className="relative m-4 w-80 h-48 rounded-lg shadow-md overflow-hidden transform transition-all duration-300 hover:scale-105"
+					>
+						<Link href={`/blog/${post.title}`}>
+							<div className="flex flex-col justify-between h-full">
+								<div className="relative h-32">
+									<Image
+										src={`/${post.image}`}
+										alt={post.title}
+										layout="fill"
+										objectFit="cover"
+										className="rounded-t-lg"
+									/>
+								</div>
+								<div className="p-4 flex-1">
+									<h2 className="text-lg font-bold text-redCandy-black">
+										{post.title}
+									</h2>
+								</div>
+								<div className="absolute top-0 left-0 w-full h-full bg-redCandy-black bg-opacity-80 opacity-0 hover:opacity-100 flex justify-center items-center p-4 text-redCandy-white text-center">
+									<p>{post.description}</p>
+								</div>
+							</div>
+						</Link>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
