249e3ad57f60be7920815d48ee5165048e9dc316
subhamc88 <subham020304@gmail.com>
2024-08-24 12:52:58 +0530

    page loading style changed

--- app/blog/[posts]/page.tsx
+++ app/blog/[posts]/page.tsx
@@ -1,32 +1,44 @@
-import path from 'path';
-import { marked } from 'marked';
-import matter from 'gray-matter';
-import getPostMetadata from '../../../utils/getPostMetadata';
-import getFilePath from '../../../utils/getFilePath';
+import path from 'path'
+import { marked } from 'marked'
+import matter from 'gray-matter'
+import getPostMetadata from '../../../utils/getPostMetadata'
+import getFilePath from '../../../utils/getFilePath'
+import Image from 'next/image'
 
-const postDir = path.join(process.cwd(), '/posts');
+const postDir = path.join(process.cwd(), '/posts')
+const filePath = getFilePath(postDir)
 
-// Generate static paths for dynamic routing
 export async function generateStaticParams() {
-	const filePath = getFilePath(postDir);
-	const metadata = getPostMetadata(filePath);
-	const postsLinks = metadata.map((post) => ({ posts: post.slug }));
-	return postsLinks;
+	const metadata = getPostMetadata(filePath)
+	const postsLinks = metadata.map((post) => ({ posts: post.slug }))
+	return postsLinks
 }
 
-// Fetch data directly in the component
-export default async function Posts({ params }: { params: { posts: string } }) {
-	const filePath = getFilePath(postDir);
-	const targetFile = filePath.find(file => file.endsWith(`${params.posts}.md`));
-
-	if (!targetFile) {
-		return <div>Post not found</div>;
+export default async function Posts(props: any) {
+	const title = decodeURI(props.params.posts) + '.md'
+	for (const file of filePath) {
+		const fileName = path.basename(file);
+		if (fileName === title) {
+			const { data, content } = matter.read(file)
+			const contentHtml = marked(content)
+			return (
+				<div className="bg-redCandy-white text-redCandy-black min-h-screen">
+					{data.image && (
+						<Image
+							src={`/${data.image}`}
+							alt={data.title}
+							width={1920}
+							height={1080}
+							className="shadow-md"
+						/>
+					)}
+					<h1 className="text-3xl font-bold text-center z-50">{data.title}</h1>
+					<div
+						dangerouslySetInnerHTML={{ __html: contentHtml }}
+						className="prose lg:prose-xl bg-redCandy-white p-8 rounded-lg shadow-lg text-redCandy-black"
+					/>
+				</div>
+			)
+		}
 	}
-
-	const { content } = matter.read(targetFile);
-	const contentHtml = marked(content);
-
-	return (
-		<div dangerouslySetInnerHTML={{ __html: contentHtml }} className="overflow-y-scroll h-screen" />
-	);
 }
