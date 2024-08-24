b3abc9bb60d471c6ea63ba32ff5966c9c5ec1517
subhamc88 <subham020304@gmail.com>
2024-08-24 12:53:36 +0530

    next.config.js image extension added for unoptimization

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-module.exports = {
+export default {
   reactStrictMode: true,
   output: "export",
+  images: {
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    imageSizes: [16, 32, 48, 64, 96],
+    formats: ["image/webp"],
+    unoptimized: true,
+  },
 };
