45bc328b349e7e041715b86675ea1948319d5c1c
subhamc88 <subham020304@gmail.com>
2024-09-03 09:32:02 +0530

    conditional rendering to show posts based on draft metadata

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -8,38 +8,45 @@ export default function Blog() {
 	const postDir = path.join(process.cwd(), '/posts');
 	const filePath = getFilePath(postDir);
 	const metadata = getPostMetadata(filePath);
+	const publishedPosts = metadata.filter(post => post.draft);
 
-	return (
-		<div className="text-redCandy-black container mx-auto p-4 flex flex-wrap justify-center items-center min-h-screen">
-			<h1 className="text-4xl font-bold text-redCandy-black w-full text-center mb-12">
-				Welcome to My Space
-			</h1>
-			<ul className="w-full flex flex-wrap justify-center gap-8">
-				{metadata.map((post) => (
-					<li
-						key={post.title}
-						className="relative aspect-w-16 aspect-h-9 rounded-3xl shadow-lg overflow-hidden transform transition-all duration-300 hover:scale-105 hover:shadow-xl"
-					>
-						<Link href={`/blog/${post.slug}`}>
-							<Image
-								src={`/${post.image}`}
-								alt={post.title}
-								width={320}
-								height={180}
-							/>
-							<div className="p-4 flex flex-col h-full bg-white rounded-b-3xl z-40">
-								<h2 className="text-xl font-semibold text-redCandy-black mb-2 truncate">
-									{post.title}
-								</h2>
-								<span className="text-sm text-gray-600 mb-2">{post.date}</span>
-								<div className="absolute inset-0 bg-black bg-opacity-60 opacity-0 hover:opacity-100 flex justify-center items-center p-4 text-white text-center transition-opacity duration-300 rounded-b-3xl">
-									<p className="text-sm">{post.description || 'No description available'}</p>
-								</div>
-							</div>
-						</Link>
-					</li>
-				))}
-			</ul>
-		</div>
-	);
+return (
+    <div className="text-redCandy-black container mx-auto p-4 flex flex-wrap justify-center items-center min-h-screen">
+      <h1 className="text-4xl font-bold text-redCandy-black w-full text-center mb-12">
+        Welcome to My Space
+      </h1>
+      <ul className="w-full flex flex-wrap justify-center gap-8">
+        {publishedPosts.length ? (
+          publishedPosts.map(({ title, slug, image, date, description }) => (
+            <li
+              key={slug}
+              className="relative aspect-w-16 aspect-h-9 rounded-3xl shadow-lg overflow-hidden transform transition-all duration-300 hover:scale-105 hover:shadow-xl"
+            >
+              <Link href={`/blog/${slug}`}>
+                <a>
+                  <Image
+                    src={`/${image}`}
+                    alt={title}
+                    width={320}
+                    height={180}
+                  />
+                  <div className="p-4 flex flex-col h-full bg-white rounded-b-3xl z-40">
+                    <h2 className="text-xl font-semibold text-redCandy-black mb-2 truncate">
+                      {title}
+                    </h2>
+                    <span className="text-sm text-gray-600 mb-2">{date}</span>
+                    <div className="absolute inset-0 bg-black bg-opacity-60 opacity-0 hover:opacity-100 flex justify-center items-center p-4 text-white text-center transition-opacity duration-300 rounded-b-3xl">
+                      <p className="text-sm">{description || 'No description available'}</p>
+                    </div>
+                  </div>
+                </a>
+              </Link>
+            </li>
+          ))
+        ) : (
+          <p className="text-center w-full">No posts available.</p>
+        )}
+      </ul>
+    </div>
+  );
 }
