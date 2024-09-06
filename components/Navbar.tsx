9293d91e6db20434dd61ca85973069736ee472c6
subhamc88 <subham020304@gmail.com>
2024-09-06 10:23:38 +0530

    navbar tried to emulate dock

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,7 +25,7 @@ export default function Navbar() {
 				role="navigation"
 				aria-label="Mobile/Tablet Navigation"
 			>
-				<ul className="bg-catpuccin-softGray-100/80 backdrop-blur-md flex justify-around py-3 px-5 rounded-2xl shadow-lg">
+				<ul className="bg-catpuccin-softGray backdrop-blur-md flex justify-around w-full py-7 rounded-3xl">
 					{iconList.map((item) => (
 						<li key={item.path} className="flex flex-col items-center">
 							<Link href={item.path}>
